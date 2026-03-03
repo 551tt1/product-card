62a4461f190046b1001ffc594dcce473792807fa
Ilyas <reddlt@list.ru>
2026-03-03 09:56:15 +0300

    fixed file homework-6.js

--- homework-6.js
+++ homework-6.js
@@ -17,7 +17,7 @@ const car = {
   color: "black",
   transmission: "Manual transmission",
 };
-car.ownerName = person.name;
+car.owner = person.name;
 
 //6.5
 function setMaxSpeed(objectName) {
@@ -28,82 +28,82 @@ function setMaxSpeed(objectName) {
 setMaxSpeed(car);
 
 //6.6
-function propertyValueOutput(objectName, objectProperty) {
+function outputPropertyValue(objectName, objectProperty) {
   console.log(this.objectProperty);
 }
 //6.7
 const array = ["Bread", "Milk", "Meat", "Cheese"];
 //6.8
 const library = [
-  (firstBook = {
+  firstBook = {
     bookName: "1984",
     bookAuthor: "Джордж Оруэлл",
     bookPublicationYear: 1949,
     bookCoverColor: "серый",
     bookGenre: "антиутопия",
-  }),
-  (secondBook = {
+  },
+  secondBook = {
     bookName: "Гордость и предубеждение",
     bookAuthor: "Джейн Остин",
     bookPublicationYear: 1813,
     bookCoverColor: "светло-зелёный",
     bookGenre: "роман",
-  }),
-  (thirdBook = {
+  },
+  thirdBook = {
     bookName: "Война и мир",
     bookAuthor: "Лев Толстой",
     bookPublicationYear: 1869,
     bookCoverColor: "тёмно-зелёный",
     bookGenre: "исторический роман",
-  }),
-  (forhBook = {
+  },
+  forhBook = {
     bookName: "Преступление и наказание",
     bookAuthor: "Фёдор Достоевский",
     bookPublicationYear: 1866,
     bookCoverColor: "бордовый",
     bookGenre: "психологический роман",
-  }),
-  (fifthBook = {
+  },
+  fifthBook = {
     bookName: "451 градус по Фаренгейту",
     bookAuthor: "Рэй Брэдбери",
     bookPublicationYear: 1953,
     bookCoverColor: "оранжевый",
     bookGenre: "научная фантастика",
-  }),
+  },
 ];
 
 library.push(
-  (sixthBook = {
+  sixthBook = {
     bookName: "Старик и море",
     bookAuthor: "Эрнест Хемингуэй",
     bookPublicationYear: 1952,
     bookCoverColor: "синий",
     bookGenre: "повесть",
-  }),
+  },
 );
 //6.9
 const library2 = [
-  (firstStarBook = {
+  firstStarBook = {
     bookName: "Наследник Империи",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1991,
     bookCoverColor: "чёрный",
     bookGenre: "sci-fi",
-  }),
-  (secondStarBook = {
+  },
+  secondStarBook = {
     bookName: "Тёмная сила восстаёт",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1992,
     bookCoverColor: "тёмно-серый",
     bookGenre: "sci-fi",
-  }),
-  (thirdStarBook = {
+  },
+  thirdStarBook = {
     bookName: "Последний полёт «Звезды Смерти»",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1993,
     bookCoverColor: "красный",
     bookGenre: "sci-fi",
-  }),
+  },
 ];
 
 const bigLibrary = library.concat(library2);
@@ -111,8 +111,8 @@ const bigLibrary = library.concat(library2);
 //6.10
 const rareLibrary = bigLibrary.map(function (element, index, array) {
   if (array[index].bookPublicationYear <= 1950) {
-    return (array[index] = { ...array[index], isRare: true });
+    return array[index] = { ...array[index], isRare: true };
   } else {
-    return (array[index] = { ...array[index], isRare: false });
+    return array[index] = { ...array[index], isRare: false };
   }
 });
