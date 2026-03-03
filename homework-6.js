c9c1ac3629dd5437c2a42339ad11367bd4d04abc
Ilyas <reddlt@list.ru>
2026-03-03 22:26:46 +0300

    fixed comments

--- homework-6.js
+++ homework-6.js
@@ -35,35 +35,35 @@ function outputPropertyValue(objectName, objectProperty) {
 const array = ["Bread", "Milk", "Meat", "Cheese"];
 //6.8
 const library = [
-  firstBook = {
+  {
     bookName: "1984",
     bookAuthor: "Джордж Оруэлл",
     bookPublicationYear: 1949,
     bookCoverColor: "серый",
     bookGenre: "антиутопия",
   },
-  secondBook = {
+  {
     bookName: "Гордость и предубеждение",
     bookAuthor: "Джейн Остин",
     bookPublicationYear: 1813,
     bookCoverColor: "светло-зелёный",
     bookGenre: "роман",
   },
-  thirdBook = {
+  {
     bookName: "Война и мир",
     bookAuthor: "Лев Толстой",
     bookPublicationYear: 1869,
     bookCoverColor: "тёмно-зелёный",
     bookGenre: "исторический роман",
   },
-  forhBook = {
+  {
     bookName: "Преступление и наказание",
     bookAuthor: "Фёдор Достоевский",
     bookPublicationYear: 1866,
     bookCoverColor: "бордовый",
     bookGenre: "психологический роман",
   },
-  fifthBook = {
+  {
     bookName: "451 градус по Фаренгейту",
     bookAuthor: "Рэй Брэдбери",
     bookPublicationYear: 1953,
@@ -73,7 +73,7 @@ const library = [
 ];
 
 library.push(
-  sixthBook = {
+  {
     bookName: "Старик и море",
     bookAuthor: "Эрнест Хемингуэй",
     bookPublicationYear: 1952,
@@ -83,21 +83,21 @@ library.push(
 );
 //6.9
 const library2 = [
-  firstStarBook = {
+  {
     bookName: "Наследник Империи",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1991,
     bookCoverColor: "чёрный",
     bookGenre: "sci-fi",
   },
-  secondStarBook = {
+  {
     bookName: "Тёмная сила восстаёт",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1992,
     bookCoverColor: "тёмно-серый",
     bookGenre: "sci-fi",
   },
-  thirdStarBook = {
+  {
     bookName: "Последний полёт «Звезды Смерти»",
     bookAuthor: "Тимоти Захн",
     bookPublicationYear: 1993,
@@ -111,8 +111,8 @@ const bigLibrary = library.concat(library2);
 //6.10
 const rareLibrary = bigLibrary.map(function (element, index, array) {
   if (array[index].bookPublicationYear <= 1950) {
-    return array[index] = { ...array[index], isRare: true };
+    return array[index] = { ...element, isRare: true };
   } else {
-    return array[index] = { ...array[index], isRare: false };
+    return array[index] = { ...element, isRare: false };
   }
 });
