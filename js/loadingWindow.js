6d8e5ea48be684287f38b9cea94d3c7d549fa825
Ilyas <reddlt@list.ru>
2026-07-02 11:27:13 +0300

    Created new folder for the hometask

---
+++ js/loadingWindow.js
@@ -0,0 +1,20 @@
+export class loadingWindow {
+  constructor() {
+    this.create();
+  }
+  create() {
+    this.el = document.createElement('p');
+    this.el.classList.add("loading");
+    document.body.append(this.el);
+    this.el.textContent = "Loading...";
+    this.overlay = document.createElement('div');
+    this.overlay.classList.add('loading-overlay');
+    document.body.append(this.overlay);
+  }
+  windowRemove(){
+    this.overlay.remove();
+    setTimeout(()=> {
+      this.el.remove();
+    }, 60);
+  }
+}
